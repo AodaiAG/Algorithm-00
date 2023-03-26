43e381bcba805cb737417f9a9420436ad3e8a570
Sec4life.$ao <de7ket.3yonk.a@hotmail.com>
2023-03-26 22:22:42 +0300

    j

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ void main()
 
 	cout << "Is the graph directed: y/n: ";
 	cin >> isGraphDirected;
-	cout << "Please Enter The Number Of Verdicts: ";
+	cout << "Please Enter The Number Of Nodes: ";
 	cin >> NumberOfNodes;
 	cout << "Please Enter The Number Of Edges: ";
 	cin >> NumberOfEdges;
